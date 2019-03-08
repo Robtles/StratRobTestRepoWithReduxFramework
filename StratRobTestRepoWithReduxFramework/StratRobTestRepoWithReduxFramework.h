f5046232a0dd80c9c5a7a6c415cf7205c589d581
Rob <robin.dreux@bruce.work>
2019-03-08 10:47:55 +0100

    First commit

---
+++ StratRobTestRepoWithReduxFramework/StratRobTestRepoWithReduxFramework.h
@@ -0,0 +1,19 @@
+//
+//  StratRobTestRepoWithReduxFramework.h
+//  StratRobTestRepoWithReduxFramework
+//
+//  Created by Rob on 08/03/2019.
+//  Copyright © 2019 com.rob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StratRobTestRepoWithReduxFramework.
+FOUNDATION_EXPORT double StratRobTestRepoWithReduxFrameworkVersionNumber;
+
+//! Project version string for StratRobTestRepoWithReduxFramework.
+FOUNDATION_EXPORT const unsigned char StratRobTestRepoWithReduxFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StratRobTestRepoWithReduxFramework/PublicHeader.h>
+
+
