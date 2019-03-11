44fb7dc172ed581bfa5de5adee4bcb31e05ecd24
Rob <robin.dreux@bruce.work>
2019-03-11 10:52:27 +0100

    Improved AppDelegate presentation and Style

---
+++ StratRobTestRepoWithReduxFramework/Application/Application.swift
@@ -0,0 +1,181 @@
+//
+//  Application.swift
+//  StratRobTestRepoWithReduxFramework
+//
+//  Created by Rob on 11/03/2019.
+//  Copyright © 2019 com.rob. All rights reserved.
+//
+
+import UIKit
+
+
+open class Application: NSObject {
+    
+    open class Delegate: UIResponder, UIApplicationDelegate {
+        
+        open var window: UIWindow?
+        
+        open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.launchOperations()
+            return true
+        }
+        
+        open func launchOperations() {}
+        
+        // MARK: View Methods
+        
+        open func defaultLaunch() {
+            self.replace(with: UIViewController())
+        }
+        
+        open func replace(with viewController: UIViewController) {
+            defer {
+                window?.makeKeyAndVisible()
+            }
+            if Thread.isMainThread {
+                window?.rootViewController = viewController
+            } else {
+                DispatchQueue.main.async {
+                    self.window?.rootViewController = viewController
+                }
+            }
+        }
+        
+    }
+
+    public struct Style {
+        
+        public struct ActivityIndicator {
+            
+            public static var color: UIColor? {
+                didSet {
+                    UIActivityIndicatorView.appearance().tintColor = color
+                }
+            }
+            
+        }
+        
+        public struct Button {
+            
+            public static var backgroundColor: UIColor? {
+                didSet {
+                    UIButton.appearance().backgroundColor = backgroundColor
+                }
+            }
+            
+            public static var font: UIFont?
+            
+            public static var textColor: UIColor? {
+                didSet {
+                    UIButton.appearance().setTitleColor(textColor, for: .normal)
+                }
+            }
+            
+        }
+        
+        public struct NavigationBar {
+            
+            public static var backgroundColor: UIColor? {
+                didSet {
+                    UINavigationBar.appearance().barTintColor = backgroundColor
+                }
+            }
+            
+            public static var font: UIFont? {
+                didSet {
+                    if let font = font {
+                        var previousAttributes = UINavigationBar.appearance().titleTextAttributes ?? [:]
+                        previousAttributes[.font] = font
+                        UINavigationBar.appearance().titleTextAttributes = previousAttributes
+                    }
+                }
+            }
+            
+            public static var textColor: UIColor? {
+                didSet {
+                    if let textColor = textColor {
+                        var previousAttributes = UINavigationBar.appearance().titleTextAttributes ?? [:]
+                        previousAttributes[.foregroundColor] = textColor
+                        UINavigationBar.appearance().titleTextAttributes = previousAttributes
+                    }
+                }
+            }
+            
+        }
+        
+        public struct PageControl {
+            
+            public static var backgroundColor: UIColor? {
+                didSet {
+                    UIPageControl.appearance().backgroundColor = backgroundColor
+                }
+            }
+            
+            public static var currentPageIndicatorTintColor: UIColor? {
+                didSet {
+                    UIPageControl.appearance().currentPageIndicatorTintColor = currentPageIndicatorTintColor
+                }
+            }
+            
+            public static var pageIndicatorTintColor: UIColor? {
+                didSet {
+                    UIPageControl.appearance().pageIndicatorTintColor = pageIndicatorTintColor
+                }
+            }
+            
+        }
+        
+        public struct TabBar {
+            
+            public static var backgroundColor: UIColor? {
+                didSet {
+                    UITabBar.appearance().barTintColor = backgroundColor
+                }
+            }
+            
+        }
+        
+        public struct TextField {
+            
+            public static var borderStyle: UITextField.BorderStyle? {
+                didSet {
+                    UITextField.appearance().borderStyle = borderStyle ?? .line
+                }
+            }
+            
+            public static var font: UIFont? {
+                didSet {
+                    UITextField.appearance().font = font
+                }
+            }
+            
+            public static var textColor: UIColor? {
+                didSet {
+                    UITextField.appearance().textColor = textColor
+                    UITextField.appearance().tintColor = textColor
+                }
+            }
+            
+        }
+        
+        public struct TextView {
+            
+            public static var font: UIFont? {
+                didSet {
+                    UITextView.appearance().font = font
+                }
+            }
+            
+            public static var textColor: UIColor? {
+                didSet {
+                    UITextView.appearance().textColor = textColor
+                    UITextView.appearance().tintColor = textColor
+                }
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ StratRobTestRepoWithReduxFramework/SDK/Basis/Extensions/UIButton+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UIButton+Extension.swift
+//  StratRobTestRepoWithReduxFramework
+//
+//  Created by Rob on 11/03/2019.
+//  Copyright © 2019 com.rob. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIButton {
+    
+    /// START OF CRAZY TRICK FOR UIBUTTON
+    // Since Apple does not provide a way to easily update the font of all the buttons throughout the app,
+    // and UILabel.appearance(whenContainedIn: [UIButton.self]).font seems to be broken,
+    // a workaround consists in updating the font in awakeFromNib() and specifying if this button should
+    // use the proxy appearance font. And since extensions may not contain stored properties, the
+    // way to go consists in using a computed property instead and play around with the pointer address.
+    private static var _shouldUseProxyAppearanceFont = [String: Bool]()
+    
+    var shouldUseProxyAppearanceFont: Bool {
+        get {
+            let tmpAddress = String(format: "%p", unsafeBitCast(self, to: Int.self))
+            return UIButton._shouldUseProxyAppearanceFont[tmpAddress] ?? true
+        }
+        set(newValue) {
+            let tmpAddress = String(format: "%p", unsafeBitCast(self, to: Int.self))
+            UIButton._shouldUseProxyAppearanceFont[tmpAddress] = newValue
+        }
+    }
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        if let font = Application.Style.Button.font, self.shouldUseProxyAppearanceFont {
+            self.titleLabel?.font = font
+        }
+    }
+    /// END OF CRAZY TRICK
+    
+}
